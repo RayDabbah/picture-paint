3058f828d1b6264665d6629e18e2bab7dfa804d6
raydabbah <raydabbah@gmail.com>
2020-07-26 10:20:15 -0400

    picker button style

--- src/components/ColorPicker.vue
+++ src/components/ColorPicker.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="picker" ref="picker">Click Here To Show</div>
+    <div :style="{background: selectedColor || '#000000' }" ref="picker" class="picker">
+        <span class="picker-text">Click Here To Select a color!</span></div>
 </template>
 
 <script>
@@ -9,19 +10,39 @@
     export default {
         name: "ColorPicker",
         setup(props, {emit}) {
-            const picker = ref(null)
+            const picker = ref(null);
+            const selectedColor = ref(null);
+
             onMounted(() => {
                 new Picker({
                     alpha: false,
-                    parent: picker.value, onChange: color => emit('change', color.hex)
+                    parent: picker.value, onChange: color => {
+                        emit('change', color.hex);
+                        selectedColor.value = color.hex;
+                    }
 
                 })
             });
-            return {picker}
+            return {picker, selectedColor}
         },
     }
 </script>
 
 <style scoped>
+    .picker {
+        padding: .75rem;
+        border-radius: 10px;
+        cursor: pointer;
+        transition: 300ms;
+    }
 
+    .picker:hover {
+        opacity: .75;
+        transform: scale(1.01);
+    }
+
+    .picker-text {
+        mix-blend-mode: difference;
+        color: white;
+    }
 </style>
